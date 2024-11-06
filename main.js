df37b6c6bc1add350d275f6cca4f51388d2c47a4
soumyabrata <soumyabratam6@gmail.com>
2024-11-06 07:35:20 +0530

    all file dded

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+const { error } = require('console');
 const fs = require('fs');
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
-
+const fileName = "output.txt"
+fs.readFile(fileName,(error)=>{
+    if(error){
+        console.error("An error occurred while writing to the file:", err);
+        throw err;
+    }
+    console.log("Message written to file successfully.");
+})
